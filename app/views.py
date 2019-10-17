e88028bb7bdf9a3579e5d3e057fc85fb4df9318e
Alexander Manning <amanning32@gatech.edu>
2019-10-17 15:47:05 -0400

    Pulls random valid question from the API and displays all information in a table on the index page.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from flask import render_template
+import requests
+import json
 
 from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    question = requests.get('http://jservice.io/api/random')
+    questionData = json.loads(question.text)[0]
+    while (questionData["invalid_count"] == 1):
+        question = requests.get('http://jservice.io/api/random')
+        questionData = json.loads(question.text)[0]
+    return render_template("index.html", result = questionData)
 
 @app.route('/about')
 def about():
