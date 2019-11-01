c350f8c1b9713b47a401e57555517d8219339778
Alexander Manning <amanning32@gatech.edu>
2019-10-31 22:10:05 -0400

    Fixed bug when one date was given and updated the index page's header.

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ This Flask website interfaces with the jService API to display *Jeopardy!* quest
 
 **NOTE: This is a work of progress which is constantly being updated. There are incomplete features and features yet-to-be-implemented.**
 
+The master branch of this repository is automatically hosted at `https://ajeopardy.herokuapp.com/`.
+
+If you would like to set up the build locally, follow these instructions:
+
 To build, ensure that all of the dependencies in `requirements.txt` are downloaded on your machine. If you have pip, this can be done in the terminal or command line by running `pip install -r requirements.txt` in the home directory of the repository. I recommend setting up a virtual environment to ensure no conflicts with other python versions and dependencies.
 
 Then navigate to the home directory of this repository in the terminal or command line and run `flask run`. The website will then appear at `127.0.0.1:5000`.
 
 Known bugs:
-- Search only reliably works when all fields are filled.
 - No way to find category IDs natively.
 - Can only display up to 100 valid questions.
--- app/views.py
+++ app/views.py
@@ -8,11 +8,15 @@ from app import app
 def index():
     if request.method == "POST":
         # parse input into format for API call
+        # note the weirdness if one of min_date and max_date are passed but not both. jService flips them? so I flip them back
         query = 'http://jservice.io/api/clues?'
         if (len(request.form['min_date']) == 10):
-            query = query + "min_date=" + request.form['min_date'] + "&"
-        if (len(request.form['max_date']) == 10):
-            query = query + "max_date=" + request.form['max_date'] + "&"
+            if (len(request.form['max_date']) == 10):
+                query = query + "min_date=" + request.form['min_date'] + "&max_date=" + request.form['max_date'] + "&"
+            else:
+                query = query + "max_date=" + request.form['min_date'] + "&"
+        elif (len(request.form['max_date']) == 10):
+            query = query + "min_date=" + request.form['max_date'] + "&"
         if (request.form['category'] != ""):
             query = query + "category=" + request.form['category'] + "&"
         if (request.form['value'] != "Select value"):
@@ -20,24 +24,37 @@ def index():
 
         query = query[:-1] # either trailing ? or trailing &
 
-        question = requests.get(query)
+        question = requests.get(query) # make API call
 
+        # parse return from API call; error when no questions presented
         if (len(json.loads(question.text)) == 0):
             question = requests.get('http://jservice.io/api/random')
             questionData = json.loads(question.text)
-            # while (questionData["invalid_count"] != None):
-            #     question = requests.get('http://jservice.io/api/random')
-            #     questionData = json.loads(question.text)
+
+            # make sure the random question we get is valid
+            while (questionData[0]["invalid_count"] != None):
+                question = requests.get('http://jservice.io/api/random')
+                questionData = json.loads(question.text)
+
             errorMsg = "No clues match those details. Please try again. A random clue has been provided instead."
             return render_template("index.html", result = questionData, error = errorMsg)
         else:
             questionData = json.loads(question.text)
 
-            # while (questionData["invalid_count"] != None):
-            #     question = requests.get('http://jservice.io/api/clues?value=100')
-            #     questionData = json.loads(question.text)
+            for questions in questionData:
+                if questions["invalid_count"] == None:
+                    return render_template("index.html", result = questionData, error = "")
 
-            return render_template("index.html", result = questionData, error = "")
+            question = requests.get('http://jservice.io/api/random')
+            questionData = json.loads(question.text)
+
+            # make sure the random question we get is valid
+            while (questionData[0]["invalid_count"] != None):
+                question = requests.get('http://jservice.io/api/random')
+                questionData = json.loads(question.text)
+
+            errorMsg = "No clues match those details. Please try again. A random clue has been provided instead."
+            return render_template("index.html", result = questionData, error = errorMsg)
     else: # initial load only, since any other loading is from a POST
         question = requests.get('http://jservice.io/api/random')
         questionData = json.loads(question.text)
